93281a3240f4d08520cbafe31ae321fade6cdb0f
Parker <78247679+killerclaws12@users.noreply.github.com>
2022-03-06 10:39:05 -0500

    ☎️| Updated Footer

--- src/Commands/Info/hotlines.js
+++ src/Commands/Info/hotlines.js
@@ -9,9 +9,9 @@ module.exports = new Command({
   async run(message, args, client) {
     const playlistEmbed = new MessageEmbed()
         .setTitle("HotLines")
-        .setDescription("The following commands will give you hotlines for various things! \n```-abortionlines``` \n```-abuselines``` \n```-suicidelines``` \n```-addictionlines``` \n```-cancerlines``` \n```-caregiverlines``` ")
+        .setDescription("The following commands will give you hotlines for various things! \n```-abortionlines``` \n```-abuselines``` \n```-suicidelines``` \n```-addictionlines``` \n```-cancerlines``` \n```-caregiverlines``` \n__**There are a lot of phone numbers on this bot. If you don’t know where to start, we suggest dialing 2-1-1 on your phone to be connected with the national human service call center. You can learn more about the [2-1-1 number](https://www.211.org/)**__ ")
         .setColor("PURPLE")
-        .setFooter('More hotlines coming soon!')
+        .setFooter('YOU ARE LOVED!')
         .setTimestamp()
 
         message.reply({ embeds:[playlistEmbed] })
